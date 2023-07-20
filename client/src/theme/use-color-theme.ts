64e9072168397ee18c3580b77211fe6eec7a8967
Trương Đình Kiên <truongdinhkien321@gmail.com>
2023-07-20 11:09:53 +0700

    map material locale with react-i18-next

--- client/src/theme/use-color-theme.ts
+++ client/src/theme/use-color-theme.ts
@@ -1,28 +1,28 @@
 import { createTheme, PaletteMode } from "@mui/material";
 import React from "react";
-import theme, { getDesignTokens } from "./theme";
+import { getDesignTokens } from "./theme";
+import { viVN, enUS, Localization } from "@mui/material/locale";
+import { useTranslation } from "react-i18next";
 
-export const useColorTheme = () => {
+const LangMapping: { [key: string]: Localization } = {
+  vi: viVN,
+  en: enUS,
+};
+
+export const useLocaleAndColorTheme = () => {
+  const { i18n } = useTranslation();
+
+  const keyLang = i18n.resolvedLanguage || "vi";
+  const currLang = LangMapping[keyLang] || viVN;
+
   const [mode, setMode] = React.useState<PaletteMode>("light");
 
   const toggleColorMode = () =>
     setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
 
-  // const modifiedTheme = React.useMemo(
-  //   () =>
-  //     createTheme({
-  //       ...theme,
-  //       palette: {
-  //         ...theme.palette,
-  //         mode,
-  //       },
-  //     }),
-  //   [mode]
-  // );
-
   const modifiedTheme = React.useMemo(
-    () => createTheme(getDesignTokens(mode)),
-    [mode]
+    () => createTheme(getDesignTokens(mode), currLang),
+    [mode, currLang]
   );
 
   return {
